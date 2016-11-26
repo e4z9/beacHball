e23b4e83c62cfb609faf6b3fcbc03c0da27ae5b8
Eike Ziller <git@eikeziller.de>
2016-11-26 15:25:50 +0100

    Render potatos instead of boxes
    Change rendering to draw a "scene" with "sprites" (= texture with position).
    Use lenses for easy access of the scene structure.

--- src/Main.hs
+++ src/Main.hs
@@ -1,10 +1,14 @@
 {-# LANGUAGE OverloadedStrings #-}
 {-# LANGUAGE Arrows #-}
+{-# LANGUAGE TemplateHaskell #-}
 
 module Main where
 
 import Input
 
+import Paths_beacHball
+
+import Control.Lens
 import Control.Monad.IO.Class
 import Control.Wire
 import Foreign.C.Types
@@ -27,38 +31,70 @@ pos1 = integral 50 . speedControl SDL.ScancodeA SDL.ScancodeD
 pos2 :: (HasTime t s, Monoid e, Monad m) => Wire s e m Keys CFloat
 pos2 = integral 350 . speedControl SDL.ScancodeLeft SDL.ScancodeRight
 
-logic :: (HasTime t s, Monad m) => Wire s () m [SDL.Event] RenderInfo
-logic = proc events -> do
+loadTexture :: MonadIO m => SDL.Renderer -> FilePath -> m SDL.Texture
+loadTexture renderer path = do
+  surface <- SDL.loadBMP path
+  texture <- SDL.createTextureFromSurface renderer surface
+  SDL.freeSurface surface
+  return texture
+
+data Sprite = Sprite {
+  _texture :: SDL.Texture,
+  _x :: CFloat,
+  _y :: CFloat
+}
+makeLenses ''Sprite
+
+data Scene = Scene {
+  _player1 :: Sprite,
+  _player2 :: Sprite
+}
+makeLenses ''Scene
+
+startScene :: SDL.Renderer -> IO Scene
+startScene renderer = do
+  potato1 <- loadTexture renderer =<< getDataFileName "potato_sml.bmp"
+  potato2 <- loadTexture renderer =<< getDataFileName "potato_sml2.bmp"
+  return $ Scene (Sprite potato1 50 250) (Sprite potato2 350 250)
+
+logic :: (HasTime t s, Monad m) => Wire s () m (Scene, [SDL.Event]) Scene
+logic = proc (scene, events) -> do
   untilQuitOrClose -< events
   keys <- handleKeyEvents -< events
   x1 <- pos1 -< keys
   x2 <- pos2 -< keys
-  returnA -< (x1, x2)
+  returnA -< set (player2 . x) x2 . set (player1 . x) x1 $ scene
 
 anyRenderingDriver = -1
 
 toCInt n = CInt (fromInteger $ toInteger $ round n)
 
-type RenderInfo = (CFloat, CFloat)
+renderSprite :: MonadIO m => SDL.Renderer -> Sprite -> m ()
+renderSprite r sprite = do
+  let xi = toCInt $ view x sprite
+  let yi = toCInt $ view y sprite
+  let tex = view texture sprite
+  textureInfo <- SDL.queryTexture tex
+  let wi = SDL.textureWidth textureInfo
+  let hi = SDL.textureHeight textureInfo
+  SDL.copy r tex Nothing $ Just $ SDL.Rectangle (SDL.P (SDL.V2 xi yi)) (SDL.V2 wi hi)
 
-render :: MonadIO m => RenderInfo -> SDL.Renderer-> m ()
-render (x1, x2) r = do
-  let (x1p, x2p) = (toCInt x1, toCInt x2)
-  SDL.rendererDrawColor r SDL.$= SDL.V4 0 0 0 255
-  SDL.clear r
+render :: MonadIO m => SDL.Renderer -> Scene -> m ()
+render r scene = do
   SDL.rendererDrawColor r SDL.$= SDL.V4 255 255 255 255
-  SDL.fillRect r $ Just $ SDL.Rectangle (SDL.P (SDL.V2 x1p 200)) (SDL.V2 40 40)
-  SDL.fillRect r $ Just $ SDL.Rectangle (SDL.P (SDL.V2 x2p 200)) (SDL.V2 40 40)
+  SDL.clear r
+  renderSprite r $ view player1 scene
+  renderSprite r $ view player2 scene
   SDL.present r
 
-renderLoop :: (HasTime t s, Monoid e, MonadIO m) => SDL.Renderer -> Session m s -> Wire s e m [SDL.Event] RenderInfo -> m ()
-renderLoop renderer session wire = do
+renderLoop :: (HasTime t s, Monoid e, MonadIO m) => SDL.Renderer -> Scene -> Session m s -> Wire s e m (Scene, [SDL.Event]) Scene -> m ()
+renderLoop renderer scene session wire = do
   events <- SDL.pollEvents
   (step, session') <- stepSession session
-  (output, wire') <- stepWire wire step $ Right events
-  either (const (pure ())) (\x -> do
-      render x renderer
-      renderLoop renderer session' wire')
+  (output, wire') <- stepWire wire step $ Right (scene, events)
+  either (const (pure ())) (\scene' -> do
+      render renderer scene'
+      renderLoop renderer scene' session' wire')
     output
 
 main :: IO ()
@@ -67,5 +103,6 @@ main = do
   window <- SDL.createWindow "BeacHball" SDL.defaultWindow
   let vsyncRenderer = SDL.defaultRenderer { SDL.rendererType = SDL.AcceleratedVSyncRenderer }
   renderer <- SDL.createRenderer window anyRenderingDriver vsyncRenderer
-  renderLoop renderer clockSession_ logic
+  scene <- startScene renderer
+  renderLoop renderer scene clockSession_ logic
   SDL.quit
